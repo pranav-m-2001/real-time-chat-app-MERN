7b65d77ecae96a65de935f3ab5b1b21b2e987c22
pranav-m-2001 <mpranav888@gmail.com>
2025-05-11 23:19:48 -0700

    fixed a bug

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ if(process.env.NODE_ENV === 'production'){
     express.static(path.join(__dirname, '../frontend/dist'))
 
     app.get('*', (req,res)=>{
-        res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
     })
 }
 
